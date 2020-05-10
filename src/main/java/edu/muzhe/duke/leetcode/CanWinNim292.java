6cc613f96c5738a70026efc98798033df04b6f35
wanggang11 <wanggang11@xiaomi.com>
2020-05-10 16:01:28 +0800

    添加了是否能够赢得比赛

---
+++ src/main/java/edu/muzhe/duke/leetcode/CanWinNim292.java
@@ -0,0 +1,19 @@
+package edu.muzhe.duke.leetcode;
+
+/**
+ * @author muzhe-wang on 2020/5/10.
+ */
+public class CanWinNim292 {
+
+    /**
+     * 是否能够赢得比赛
+     *
+     * @param n
+     * @return
+     */
+    public boolean canWinNim(int n) {
+
+        return n % 4 != 0;
+    }
+
+}
