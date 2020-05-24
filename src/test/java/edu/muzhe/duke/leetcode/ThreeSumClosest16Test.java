dcf326e0a34a485114f494902d48b828ead1408e
wanggang11 <wanggang11@xiaomi.com>
2020-05-24 14:28:44 +0800

    添加了最近三数之和的实现。这里的实现是当前系统中的实现

---
+++ src/test/java/edu/muzhe/duke/leetcode/ThreeSumClosest16Test.java
@@ -0,0 +1,22 @@
+package edu.muzhe.duke.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author muzhe-wang on 2020/5/24.
+ */
+public class ThreeSumClosest16Test {
+
+    private ThreeSumClosest16 threeSumClosest16 = new ThreeSumClosest16();
+
+    @Test
+    public void threeSumClosest() {
+
+        int[] nums = new int[]{-1, 2, 1, -4};
+        int target = 1;
+        int i = threeSumClosest16.threeSumClosest(nums, target);
+        System.out.println(i);
+    }
+}
