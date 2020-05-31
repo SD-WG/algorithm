c72d6358d4e5a93a1bc710de98665388f0377405
wanggang11 <wanggang11@xiaomi.com>
2020-05-31 11:56:43 +0800

    添加了后续遍历的大小

--- src/main/java/edu/muzhe/duke/leetcode/tree/BstToGst1038.java
+++ src/main/java/edu/muzhe/duke/leetcode/tree/BstToGst1038.java
@@ -1,5 +1,7 @@
 package edu.muzhe.duke.leetcode.tree;
 
+import java.util.Stack;
+
 /**
  * @author muzhe-wang on 2020/5/31.
  */
@@ -12,6 +14,24 @@ public class BstToGst1038 {
      */
     public TreeNode bstToGst(TreeNode root) {
 
-        return null;
+        TreeNode node = root;
+        if (node == null) {
+            return node;
+        }
+        int sum = 0;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.isEmpty() || node != null) {
+            if (node != null) {
+                stack.push(node);
+                node = node.right;
+            } else {
+                node = stack.pop();
+                sum = node.val + sum;
+                node.val = sum;
+                node = node.left;
+            }
+        }
+
+        return root;
     }
 }
