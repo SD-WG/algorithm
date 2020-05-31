621587988a278d7f0500de0f8c9f0e73ada8359c
wanggang11 <wanggang11@xiaomi.com>
2020-05-31 13:52:24 +0800

    添加了非递归的实现

--- src/main/java/edu/muzhe/duke/leetcode/tree/SumOfLeftLeaves404.java
+++ src/main/java/edu/muzhe/duke/leetcode/tree/SumOfLeftLeaves404.java
@@ -1,5 +1,7 @@
 package edu.muzhe.duke.leetcode.tree;
 
+import java.util.Stack;
+
 /**
  * @author muzhe-wang on 2020/5/31.
  */
@@ -9,24 +11,61 @@ public class SumOfLeftLeaves404 {
      * 返回所有左叶之和
      * 在遍历的过程中，如何判断是叶子节点，
      * 且为左叶子节点
+     * 如何标记做子树
      * //左中右
      */
 
     public int sumOfLeftLeaves(TreeNode root) {
         //这里可以传递一个参数进行，如果这里是左子树，且左右孩子都为空，就返回
-        return sumOfLeftLeaves(root, false);
-    }
-
-    private int sumOfLeftLeaves(TreeNode root, boolean isLeftNode) {
 
         if (root == null) {
             return 0;
         }
-        if (isLeftNode && root.left == null && root.right == null) {
-            return root.val;
+
+        //用先序遍历
+        int sum = 0;
+        Stack<Pair> stack = new Stack<Pair>();
+        Pair pair = new Pair(root, false);
+        while (!stack.isEmpty() || pair != null) {
+            if (pair != null) {
+                stack.push(pair);
+                if (pair.node.left != null) {
+                    pair = new Pair(pair.node.left, true);
+                } else {
+                    pair = null;
+                }
+            } else {
+
+                pair = stack.pop();
+                if (pair.isLeftNode && pair.node.left == null && pair.node.right == null) {
+                    sum += pair.node.val;
+                }
+                if (pair.node.right != null) {
+                    pair = new Pair(pair.node.right, false);
+                } else {
+                    pair = null;
+                }
+            }
         }
-        return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+        return sum;
 
     }
+
+    /**
+     * 节点的node
+     */
+    private class Pair {
+
+        TreeNode node;
+
+        boolean isLeftNode;
+
+        public Pair(TreeNode node, boolean isLeftNode) {
+            this.node = node;
+            this.isLeftNode = isLeftNode;
+        }
+
+    }
+
 
 }
