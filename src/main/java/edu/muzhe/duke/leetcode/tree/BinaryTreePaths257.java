b95932b4a0861b88e9175f9e5fc6b6abbe51a221
wanggang11 <wanggang11@xiaomi.com>
2020-05-31 20:35:36 +0800

    这里可以使用递归的方式来实现

--- src/main/java/edu/muzhe/duke/leetcode/tree/BinaryTreePaths257.java
+++ src/main/java/edu/muzhe/duke/leetcode/tree/BinaryTreePaths257.java
@@ -10,6 +10,8 @@ public class BinaryTreePaths257 {
 
     /**
      * 二叉树的所有的路径
+     * <p>
+     * 从第一个开始
      *
      * @param root
      * @return
@@ -21,47 +23,35 @@ public class BinaryTreePaths257 {
          * 这个使用回溯算法来试下
          */
         List<String> result = new ArrayList<>();
-        List<TreeNode> temp = new ArrayList<>();
-        backtrack(root, temp, result);
+        allBinaryTreePaths(root, "", result);
         return result;
 
     }
 
     /**
-     * 回溯当前节点
-     * 处理当前节点：如果root节点是
+     * 找出素有的pant
+     *
      *
      * @param root
-     * @param temp
+     * @param path
      * @param result
      */
-    private void backtrack(TreeNode root, List<TreeNode> temp, List<String> result) {
+    private void allBinaryTreePaths(TreeNode root, String path, List<String> result) {
 
         if (root == null) {
             return;
         }
 
-        if (root.right == null && root.left == null) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < temp.size(); i++) {
-                stringBuilder.append(temp.get(i).val).append("->");
-            }
-            stringBuilder.append(root.val);
-            result.add(stringBuilder.toString());
-            return;
+        path += String.valueOf(root.val);
+        if (root.left == null && root.right == null) {
+            result.add(path);
+        } else {
+            path += "->";
+            allBinaryTreePaths(root.left, path, result);
+            allBinaryTreePaths(root.right, path, result);
         }
-
-        temp.add(root);
-        if (root.left != null) {
-            backtrack(root.left, temp, result);
-        }
-
-        if (root.right != null) {
-            backtrack(root.right, temp, result);
-        }
-        temp.remove(temp.size() - 1);
-
 
     }
+
 
 }
