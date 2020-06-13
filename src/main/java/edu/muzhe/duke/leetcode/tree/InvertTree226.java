cbd3be321c79092fe8617fe9850c31ab0a4826a9
wanggang11 <wanggang11@xiaomi.com>
2020-06-13 23:51:13 +0800

    递归的实现,将类添加到对象中去

--- src/main/java/edu/muzhe/duke/leetcode/tree/InvertTree226.java
+++ src/main/java/edu/muzhe/duke/leetcode/tree/InvertTree226.java
@@ -1,5 +1,8 @@
 package edu.muzhe.duke.leetcode.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author muzhe-wang on 2020/6/13.
  */
@@ -16,12 +19,25 @@ public class InvertTree226 {
         if (root == null) {
             return null;
         }
+        TreeNode finalRoot = root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
 
-        TreeNode newRoot = new TreeNode(root.val);
-        newRoot.left = invertTree(root.right);
-        newRoot.right = invertTree(root.left);
-        return newRoot;
+        while (!queue.isEmpty()) {
+            root = queue.poll();
+            TreeNode left = root.left;
+            root.left = root.right;
+            root.right = left;
+            if (root.left != null) {
+                queue.add(root.left);
+            }
+            if (root.right != null) {
+                queue.add(root.right);
+            }
 
+        }
+        return finalRoot;
     }
+
 
 }
